489767a97f8f049413ed3fa0fb42bae99d743627
Liviu Cornea <liviucornea@Livius-MacBook-Pro.local>
2022-09-18 17:19:29 -0400

    Smal change for empty out messages

--- src/app/components/EntryForm/EntryForm.tsx
+++ src/app/components/EntryForm/EntryForm.tsx
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {ChildForm} from "./ChildForm/ChildForm";
+import {useDispatch} from "react-redux";
+import {emptyOutMessages} from "../message-collector/mesagesSlice";
 import * as _ from 'lodash';
 // import {compose , pipe} from 'lodash/fp'
 
@@ -8,12 +10,20 @@ type Props = { formName: string }
 export function EntryForm(props?: Props) {
     let mainCssClass = 'container';
     const myObj = { name: 'Liviu'};
+    const dispatch = useDispatch();
     const mySecondObj =  _.cloneDeep(myObj);
     const [nameFromChild, setNameFromChild] = useState('');
 
+    useEffect(() => {
+        dispatch( emptyOutMessages(true));
+        // eslint-disable-next-line 
+    }, []);
+
+    
     const handleFromChild = (name: string  ) => {
         setNameFromChild(name);
     }
+
     return (<div className={mainCssClass}>
         <div><span>Entry Form component in progress</span></div>
         <div>Lodash used here {mySecondObj.name}</div>
